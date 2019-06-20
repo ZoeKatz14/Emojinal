2c8d7683e7cc154f03985f22278808359b32f3cc
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-20 15:38:50 -0500

    Iteration 2 Complete

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Alert", message: "You should go to this place!", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "On my way!", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
