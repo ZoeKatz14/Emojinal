e870ec662723892b806ba22a24c9a69aed491037
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-20 16:19:36 -0500

    iteration 2 done

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let emojis = ["🌍":"globe","🌋":"volcano","🏖":"beach","🏜":"desert","🏰":"castle"]
+    let customMessages = ["globe" : "You should check out Capetown, South Africa!", "volcano":"Try Hawaii! 🏝🏝", "beach":"What about Key West! 🏄🏄","desert":"Death Valley is always a hit! 🐪🐪", "castle":"Go back in time to Chartres, France! 🤴🤴"]
+    
     @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Alert", message: "You should go to this place!", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "On my way!", style: UIAlertAction.Style.default, handler: nil))
+        let selectedEmotion = sender.titleLabel?.text
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]
+        let alertController = UIAlertController(title: selectedEmotion, message: "", preferredStyle: UIAlertController.Style.alert)
+                alertController.addAction(UIAlertAction(title: "On my way!", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
 
