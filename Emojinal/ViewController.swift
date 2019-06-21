f1fd30ac29219d92131a6aacf2b317ad5b691129
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-21 09:39:17 -0500

    complete

--- Emojinal/ViewController.swift
+++ Emojinal/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     let emojis = ["🌍":"globe","🌋":"volcano","🏖":"beach","🏜":"desert","🏰":"castle"]
-    let customMessages = ["globe" : "You should check out Capetown, South Africa!", "volcano":"Try Hawaii! 🏝🏝", "beach":"What about Key West! 🏄🏄","desert":"Death Valley is always a hit! 🐪🐪", "castle":"Go back in time to Chartres, France! 🤴🤴"]
+    let customMessages = [
+        "globe" : ["You should check out Capetown, South Africa! 🇿🇦🇿🇦","Go to Moscow! 🇷🇺🇷🇺", "Rio de Janerio is cool! 🇧🇷🇧🇷"],
+        "volcano" : ["Try Hawaii! 🏝🏝","Mt. Vesuvius might work! 🇮🇹🇮🇹","Mt. Saint Helens is pretty cool! 🇺🇸🇺🇸"],
+        "beach" : ["What about Key West! 🏄🏄","Blue Lagoon, Turkey is stunning. 💎💎","Fiji might be for you! 🇫🇯🇫🇯"],
+        "desert" : ["Death Valley is always a hit! 🐪🐪","Check out the Sahara! 🐫🐫","Maybe Antartica? 🇦🇶🇦🇶"],
+        "castle" : ["Go back in time to Chartres, France! 🤴🤴","What about Himeji Castle in Japan? 🗾🗾", "Try Lakshamana Temple in India! 🇮🇳🇮🇳"]
+    ]
     
     @IBAction func showMessage(sender: UIButton) {
         let selectedEmotion = sender.titleLabel?.text
-        let emojiMessage = customMessages[emojis[selectedEmotion!]!]
+        let random = Int.random(in: 0 ... 2)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[random]
         let alertController = UIAlertController(title: selectedEmotion, message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
                 alertController.addAction(UIAlertAction(title: "On my way!", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
